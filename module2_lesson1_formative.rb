ec6b9e51b167a41984302a25be5a5f99c04e7683
Simon L. Mackinnon <simon.mackinnon15@gmail.com>
2016-12-12 20:18:34 +1100

    edited ruby file for practice assignment 1, change if else to case

--- module2_lesson1_formative.rb
+++ module2_lesson1_formative.rb
@@ -1,15 +1,9 @@
 some_var = "false"
 another_var = "nil"
 
-if some_var == "pink elephant"
-  puts "Don't think about the pink elephant!"
-
-elsif another_var.nil?
-  puts "Question mark in the method name?"
-
-elsif some_var == false
-  puts "Looks like this one should execute"
-
-else
-  puts "I guess nothing matched... But why?"
+case 
+when some_var == "pink elephant" then puts "Don't think about the pink elephant!"
+when another_var.nil? then puts "Question mark in the method name?"
+when some_var == false then puts "Looks like this one should execute"
+else  puts "I guess nothing matched... But why?"
 end
