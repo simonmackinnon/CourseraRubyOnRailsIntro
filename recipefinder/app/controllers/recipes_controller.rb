e12a5d2ca0848d13e7d8fe7ac3c57df6e805fced
Simon L. Mackinnon <simon.mackinnon15@gmail.com>
2016-12-22 08:37:58 +1100

    final cut of files

--- recipefinder/app/controllers/recipes_controller.rb
+++ recipefinder/app/controllers/recipes_controller.rb
@@ -1,6 +1,7 @@
 class RecipesController < ApplicationController
   def index
-  	@search_term = 'chocolate'
+  	@search_term = params[:search] || 'chocolate'
   	@recipes = Recipe.for(@search_term)
   end
 end
+			
